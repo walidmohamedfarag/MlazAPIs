02346f54a83d06ab40a71bbc0180c763287261ea
walidmohamedfarag <walid492003@gmail.com>
2026-01-30 02:03:19 +0200

    enhencment in code

--- MlazAPIs/Reposatories/IReposatory.cs
+++ MlazAPIs/Reposatories/IReposatory.cs
@@ -4,24 +4,24 @@ namespace MlazAPIs.Reposatories
 {
     public interface IReposatory<TEntity> where TEntity : class
     {
-        public Task<IEnumerable<TEntity>> GetAllAsync
+        Task<IEnumerable<TEntity>> GetAllAsync
             (
             Expression<Func<TEntity, bool>>? expression = null,
             Expression<Func<TEntity, object>>[]? includes = null,
             bool tracking = true,
             CancellationToken cancellationToken = default
             );
-        public Task<TEntity> GetOneAsync
+        Task<TEntity> GetOneAsync
             (
             Expression<Func<TEntity , bool>>? expression = null,
             Expression<Func<TEntity , object>>[]? includes = null,
             bool tracking = true,
             CancellationToken cancellationToken = default
             );
-        public Task AddAsync(TEntity entity , CancellationToken cancellationToken = default);
-        public void Update(TEntity entity , CancellationToken cancellationToken = default);
-        public void Delete(TEntity entity , CancellationToken cancellationToken = default);
-        public Task CommitAsync(CancellationToken cancellationToken = default);
+        Task AddAsync(TEntity entity , CancellationToken cancellationToken = default);
+        void Update(TEntity entity , CancellationToken cancellationToken = default);
+        void Delete(TEntity entity , CancellationToken cancellationToken = default);
+        Task CommitAsync(CancellationToken cancellationToken = default);
 
     }
 }
