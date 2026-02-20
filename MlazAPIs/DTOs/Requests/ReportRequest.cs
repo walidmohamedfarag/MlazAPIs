2a8d9eebb8dfd6c30188e830214d86d0dacf2d22
walidmohamedfarag <walid492003@gmail.com>
2026-02-20 22:07:30 +0200

    add report request

---
+++ MlazAPIs/DTOs/Requests/ReportRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MlazAPIs.DTOs.Requests
+{
+    public class ReportRequest
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+        public IFormFile? Image { get; set; } 
+        [Required]
+        public int Latitude { get; set; }
+        [Required]
+        public int Longitude { get; set; }
+
+    }
+}
