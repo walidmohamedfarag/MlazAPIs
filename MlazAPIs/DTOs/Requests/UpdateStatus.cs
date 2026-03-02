acd2c7985f0e9efa4a2eca445900b41bda92d0ec
walidmohamedfarag <walid492003@gmail.com>
2026-03-03 01:19:22 +0200

    add update status request

---
+++ MlazAPIs/DTOs/Requests/UpdateStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace MlazAPIs.DTOs.Requests
+{
+    public class UpdateStatus
+    {
+        public int Id { get; set; }
+        public string? Message { get; set; }
+        public bool? IsApproved { get; set; }
+    }
+}
